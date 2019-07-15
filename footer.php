f95f79047fad21c3f3557c3fce52ff9994cc7b92
ADinushika <amalidinushika93@gmail.com>
2019-07-15 14:10:17 +0530

    #19 Create Footer

---
+++ footer.php
@@ -0,0 +1,72 @@
+<section id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="col-md-4">
+                    <h3>LEGAL INFORMATION</h3>
+                    <p><a href="">Terms of use</a></p>
+                    <p><a href="">Data protection</a></p>
+                    <p><a href="">Cookies</a></p>
+                    <p><a href="">Imprint</a></p>
+
+                </div>
+                <div class="col-md-4">
+                    <h3>MORE PAGES</h3>
+                    <p><a href="">Jobs</a></p>
+                    <p><a href="">Partners</a></p>
+                    <p><a href="">Apps</a></p>
+                    <p><a href="">Annual Report</a></p>
+                </div>
+                <div class="col-md-4 footer-zone">
+                    <h3>CONTACTS</h3>
+                    <p><a href="">Address</a></p>
+                    <p><a href="">FAQs</a></p>
+                    <p><a href="">Media Corner</a></p>
+                    <p><a href="">Trade Corner</a></p> 
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12" style="margin-top: 50px;">
+                <div class="col-md-4">
+                    <h5>Newsletter</h5>
+                    <form class="newsletterfooter">
+                        <input type="text">
+                        <input type="submit">
+                        <a href=""><i class="fa fa-envelope"></i></a>
+                    </form>
+                </div>
+                <div class="col-md-4 footer-zone">
+                    <div class="socialfooter ">
+                        <h5>Follow Us</h5>
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-linkedin"></i></a>
+                        <a href="#"><i class="fa fa-pinterest"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-youtube"></i></a>
+                    </div> 
+                </div>
+                <div class="">
+                    
+                </div>
+            </div> 
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="col-xs-6 copyright">Grand Tour © 2019 All Rights Reserved </div>
+                    <div class="col-xs-6 payment-card">
+                        <i class="fa fa-2x fa-cc-visa"></i>
+                        <i class="fa fa-2x fa-cc-mastercard"></i>
+                        <i class="fa fa-2x fa-cc-amex"></i>
+                        <i class="fa fa-2x fa-cc-paypal"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
